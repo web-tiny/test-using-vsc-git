c611a6e29e7033ac920151d481d6d8f84e75becf
焦仁贵 <tiny.jiao@aliyun.com>
2020-03-31 22:53:01 +0800

    add a vue file

---
+++ test/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{data}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {
+      data: 'comonents'
+    }
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
